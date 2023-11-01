c3149733e979f205c414f825fd667bd2baad914c
Valerio Magnago <valerio.magnago@gmail.com>
2023-11-01 10:24:28 +0100

    WIP

---
+++ environments/LICENSE.txt
@@ -0,0 +1,3 @@
+This model is licensed under Creative Commons Attribution Non Commercial 4.0 International
+
+Author: Fernando Cañadas Aránega <fernando.ca@ual.es> (University of Almería)
---
+++ launch/run.launch.py
@@ -0,0 +1,45 @@
+# ROS2 launch file
+
+from launch import LaunchDescription
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from ament_index_python import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    mvsimDir = get_package_share_directory("maila_2d_simulation")
+    # print('mvsimDir: ' + mvsimDir)
+
+    # args that can be set from the command line or a default will be used
+    world_file_launch_arg = DeclareLaunchArgument(
+        "world_file",
+        default_value=TextSubstitution(text=os.path.join(mvsimDir, "worlds", "main.world.xml")),
+    )
+
+    headless_launch_arg = DeclareLaunchArgument("headless", default_value="False")
+
+    mvsim_node = Node(
+        package="mvsim",
+        executable="mvsim_node",
+        name="mvsim",
+        output="screen",
+        parameters=[
+            os.path.join(mvsimDir, "config", "config.yaml"),
+            {
+                "world_file": LaunchConfiguration("world_file"),
+                "headless": LaunchConfiguration("headless"),
+            },
+        ],
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", [os.path.join(mvsimDir, "mvsim_tutorial", "demo_greenhouse_ros2.rviz")]],
+    )
+
+    return LaunchDescription([world_file_launch_arg, headless_launch_arg, mvsim_node, rviz2_node])
